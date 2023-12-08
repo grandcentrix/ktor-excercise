226d0260a408105523d2b9b4a7c57d54183640e6
Dvonderstueck <dennis.vonderstueck@grandcentrix.net>
2023-12-08 10:50:49 +0100

    hello world

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,34 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
+val ktor_version = "1.5"
+val kotlin_version = "1.8.0"
+val logback_version = "1.4.2"
 
 plugins {
-    kotlin("jvm") version "1.9.21"
+    kotlin("jvm") version "1.8.0"
     id("io.ktor.plugin") version "2.3.6"
 }
 
 group = "net.grandcentrix.backend"
 version = "0.0.1"
+application {
+    mainClass.set("net.grandcentrix.backend.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-server-html-builder:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-html-builder:$ktor_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation(kotlin("test"))
 
     testImplementation(kotlin("test"))
 }
--- src/main/kotlin/net/grandcentrix/backend/Application.kt
+++ src/main/kotlin/net/grandcentrix/backend/Application.kt
@@ -1,5 +1,27 @@
 package net.grandcentrix.backend
 
-fun main() {
 
+
+import io.ktor.server.routing.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.netty.Netty
+import io.ktor.server.engine.embeddedServer
+
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+}
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
 }
