7c79c4525fda0e63885bd4e95664ca383f632738
Dvonderstueck <dennis.vonderstueck@grandcentrix.net>
2023-12-12 08:40:55 +0100

    first button code (fixed) 2.0

--- src/main/kotlin/net/grandcentrix/backend/Application.kt
+++ src/main/kotlin/net/grandcentrix/backend/Application.kt
@@ -17,21 +17,42 @@ fun Application.module() {
     configureRouting()
 }
 
+val youtubeLinks = listOf(
+    "https://www.youtube.com/watch?v=aKSxbt-O6TA",
+    "https://www.youtube.com/watch?v=7C2z4GqqS5E",
+    "https://www.youtube.com/watch?v=eaUpme4jalE",
+)
+
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondHtml {
                 head {
-                    title { +"Ktor Sample" }
+                    title { +"Ktor Test" }
                 }
                 body {
-                    h1 { +"Random Mv player!!" }
+                    h1 { +"Random MV player" }
                     button {
                         +"Click me for a random mv!"
+                        script {
+                            unsafe {
+                                raw("""
+                                    document.addEventListener('DOMContentLoaded', function() {
+                                        document.querySelector('button').onclick = function(event) {
+                                            if (event.target === this) {
+                                                alert('Button clicked!');
+                                            }
+                                        };
+                                    });
+                                """.trimIndent())
+                            }
+                        }
                     }
                 }
             }
         }
     }
 }
+
+
 
