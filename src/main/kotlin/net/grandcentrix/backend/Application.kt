6385eac573e65ca80f6215cde0a79da0f032374a
Dvonderstueck <dennis.vonderstueck@grandcentrix.net>
2024-01-12 10:41:57 +0100

    second object for saving links in memory

--- src/main/kotlin/net/grandcentrix/backend/Application.kt
+++ src/main/kotlin/net/grandcentrix/backend/Application.kt
@@ -3,8 +3,12 @@ package net.grandcentrix.backend
 import io.ktor.server.application.*
 
 fun Application.module() {
-    JsonYouTubeManager.loadYouTubeLinks()
-    configureRouting(youtubeManager = JsonYouTubeManager)
+    val youtubeManager = getYouTubeManager()
+    youtubeManager.loadYouTubeLinks()
+    configureRouting(youtubeManager = youtubeManager)
 }
 
+fun getYouTubeManager(): YouTubeManagerInterface {
+    return InMemoryYouTubeManager
 
+}
--- src/main/kotlin/net/grandcentrix/backend/YouTubeLinksHandler.kt
+++ src/main/kotlin/net/grandcentrix/backend/YouTubeLinksHandler.kt
@@ -1,6 +1,5 @@
 package net.grandcentrix.backend
 
-import java.io.File
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.Serializable
@@ -14,12 +13,12 @@ data class VideoInfo(val videoId: String, val customName: String)
 interface YouTubeManagerInterface {
 
     /**
-     * Loads YouTube links from a file.
+     * Loads YouTube links.
      */
     fun loadYouTubeLinks()
 
     /**
-     * Saves YouTube links to a file.
+     * Saves YouTube links.
      */
     fun saveYouTubeLinks()
 
@@ -50,17 +49,15 @@ interface YouTubeManagerInterface {
 
 object JsonYouTubeManager : YouTubeManagerInterface {
 
-    private val json = Json {}
-
+    private val json = Json
     private val youtubeLinks = mutableListOf<VideoInfo>()
 
     override fun getYoutubeLinks(): List<VideoInfo> {
         return youtubeLinks
     }
 
-
     override fun loadYouTubeLinks() {
-        val file = File("youtubeLinks.json")
+        val file = java.io.File("youtubeLinks.json")
         if (file.exists()) {
             youtubeLinks.clear()
             val jsonContent = file.readText()
@@ -69,7 +66,7 @@ object JsonYouTubeManager : YouTubeManagerInterface {
     }
 
     override fun saveYouTubeLinks() {
-        val file = File("youtubeLinks.json")
+        val file = java.io.File("youtubeLinks.json")
         val jsonContent = json.encodeToString(youtubeLinks)
         file.writeText(jsonContent)
     }
@@ -94,6 +91,43 @@ object JsonYouTubeManager : YouTubeManagerInterface {
         if (videoNumber >= 0 && videoNumber < youtubeLinks.size) {
             youtubeLinks.removeAt(videoNumber)
             saveYouTubeLinks()
+        }
+    }
+}
+
+object InMemoryYouTubeManager : YouTubeManagerInterface {
+
+    private val json = Json
+    private val youtubeLinks = mutableListOf<VideoInfo>()
+
+    override fun getYoutubeLinks(): List<VideoInfo> {
+        return youtubeLinks
+    }
+
+    override fun loadYouTubeLinks() {
+    }
+
+    override fun saveYouTubeLinks() {
+    }
+
+    override fun getRandomYouTubeVideoUrl(): String {
+        if (youtubeLinks.isEmpty()) {
+            return "https://www.youtube.com/"
+        }
+        val randomIndex = (0 until youtubeLinks.size).random()
+        val videoInfo = youtubeLinks[randomIndex]
+        val videoId = videoInfo.videoId
+
+        return "https://www.youtube.com/embed/$videoId"
+    }
+
+    override fun addVideo(videoId: String, customName: String) {
+        youtubeLinks.add(VideoInfo(videoId, customName))
+    }
+
+    override fun removeVideoByNumber(videoNumber: Int) {
+        if (videoNumber >= 0 && videoNumber < youtubeLinks.size) {
+            youtubeLinks.removeAt(videoNumber)
         }
     }
 }
